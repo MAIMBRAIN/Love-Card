f5a95e80ea29f6538f2dce0f368ed1cc274a8bfd
Avery Stahl <averystahl@hotmail.com>
2020-04-30 23:29:04 -0400

    Updates

---
+++ precache-manifest.eb7056b302b4942cce9bcea9822226d8.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7daa9065cdb067f072d5076003a2c1",
+    "url": "/Love-Card/index.html"
+  },
+  {
+    "revision": "883a41479cb909373db7",
+    "url": "/Love-Card/static/css/main.538360bf.chunk.css"
+  },
+  {
+    "revision": "95015602b5a179c5d20e",
+    "url": "/Love-Card/static/js/2.6e577c8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Love-Card/static/js/2.6e577c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883a41479cb909373db7",
+    "url": "/Love-Card/static/js/main.be0acb15.chunk.js"
+  },
+  {
+    "revision": "5e8c12d81d6f8b90909a",
+    "url": "/Love-Card/static/js/runtime-main.e6f329d9.js"
+  },
+  {
+    "revision": "ad3a13fc2303ad226d91f1617af0ca19",
+    "url": "/Love-Card/static/media/P_20190625_231730_BF.ad3a13fc.jpg"
+  },
+  {
+    "revision": "ca02aa2d603cfe7b7dd6e74e1228564d",
+    "url": "/Love-Card/static/media/Snapchat-1003475239.ca02aa2d.jpg"
+  },
+  {
+    "revision": "1582bbb7711354194f491ee165c1ffa8",
+    "url": "/Love-Card/static/media/Snapchat-1005719701.1582bbb7.jpg"
+  },
+  {
+    "revision": "28e36fac6ee1facd508b0501cd1ed573",
+    "url": "/Love-Card/static/media/Snapchat-1069566352.28e36fac.jpg"
+  },
+  {
+    "revision": "4a5d8ee3c4bfa2e3737f474a855fea6a",
+    "url": "/Love-Card/static/media/Snapchat-1131623588.4a5d8ee3.jpg"
+  },
+  {
+    "revision": "6ea492e666cb0b98f6c2d5e43b4a32f9",
+    "url": "/Love-Card/static/media/Snapchat-114263625.6ea492e6.jpg"
+  },
+  {
+    "revision": "e36c941320c44c5c7d16ff1202dacb29",
+    "url": "/Love-Card/static/media/Snapchat-1203605753.e36c9413.jpg"
+  },
+  {
+    "revision": "d6ab2bc37257d67f3f1197995008d369",
+    "url": "/Love-Card/static/media/Snapchat-1205220019.d6ab2bc3.jpg"
+  },
+  {
+    "revision": "c74115423194c4bf7ff8dcaa9b73ec3c",
+    "url": "/Love-Card/static/media/Snapchat-1212327580.c7411542.jpg"
+  },
+  {
+    "revision": "dc4f300229962c2014d707c161acda31",
+    "url": "/Love-Card/static/media/Snapchat-1441094795.dc4f3002.jpg"
+  },
+  {
+    "revision": "5a999202d02372fd6650e3bc0c9f3946",
+    "url": "/Love-Card/static/media/Snapchat-1660349727.5a999202.jpg"
+  },
+  {
+    "revision": "df1ffdf1bc3c79913b6e47f8e41aa93a",
+    "url": "/Love-Card/static/media/Snapchat-1730093509.df1ffdf1.jpg"
+  },
+  {
+    "revision": "96ccf1695d449a0e2bee44ae19cc44ff",
+    "url": "/Love-Card/static/media/Snapchat-1741921975.96ccf169.jpg"
+  },
+  {
+    "revision": "9a33861ba5693063a888154d862e77b7",
+    "url": "/Love-Card/static/media/Snapchat-1756650410.9a33861b.jpg"
+  },
+  {
+    "revision": "b6b0a243e02e755ba50243fa05dd8e5e",
+    "url": "/Love-Card/static/media/Snapchat-1851638814.b6b0a243.jpg"
+  },
+  {
+    "revision": "0b7f4f2e726da0dc369b75d042ecdd30",
+    "url": "/Love-Card/static/media/Snapchat-1885972695.0b7f4f2e.jpg"
+  },
+  {
+    "revision": "bdb5e462a38625524136681e714ddfec",
+    "url": "/Love-Card/static/media/Snapchat-2129790868.bdb5e462.jpg"
+  },
+  {
+    "revision": "11282e08a6334dcf3020a59240e58ed4",
+    "url": "/Love-Card/static/media/Snapchat-23877786.11282e08.jpg"
+  },
+  {
+    "revision": "8188bfdb6a2917ffbcd90e6f1364617f",
+    "url": "/Love-Card/static/media/Snapchat-244788946.8188bfdb.jpg"
+  },
+  {
+    "revision": "f7ffdd02677da330b4c73be1fc6a0216",
+    "url": "/Love-Card/static/media/Snapchat-273981528.f7ffdd02.jpg"
+  },
+  {
+    "revision": "30a40bf1dfbf7a4297604be29f204da6",
+    "url": "/Love-Card/static/media/Snapchat-292742773.30a40bf1.jpg"
+  },
+  {
+    "revision": "36ed18e6c57b4adaac247a15a8fddf41",
+    "url": "/Love-Card/static/media/Snapchat-380501927.36ed18e6.jpg"
+  },
+  {
+    "revision": "fbb853fb4be7074be1f83235283ebc03",
+    "url": "/Love-Card/static/media/Snapchat-395744754.fbb853fb.jpg"
+  },
+  {
+    "revision": "109a6c39bad4b8af457ffdf450f0867e",
+    "url": "/Love-Card/static/media/Snapchat-412390846.109a6c39.jpg"
+  },
+  {
+    "revision": "7f571a3b1194f96359edb98543b75029",
+    "url": "/Love-Card/static/media/Snapchat-437096239.7f571a3b.jpg"
+  },
+  {
+    "revision": "8250b90c5920fd1d7418a056cfb17fb6",
+    "url": "/Love-Card/static/media/Snapchat-440244283.8250b90c.jpg"
+  },
+  {
+    "revision": "1988b9d0f2b3ab65d0a65858cd68f709",
+    "url": "/Love-Card/static/media/Snapchat-443701257.1988b9d0.jpg"
+  },
+  {
+    "revision": "88ab7b0755b742c56d49c8429cd5c49a",
+    "url": "/Love-Card/static/media/Snapchat-51564828.88ab7b07.jpg"
+  },
+  {
+    "revision": "198f0e3a933364ace0586cddf8362525",
+    "url": "/Love-Card/static/media/Snapchat-534759211.198f0e3a.jpg"
+  },
+  {
+    "revision": "5b8d4fc261d80104190606329f94bf7b",
+    "url": "/Love-Card/static/media/Snapchat-59694010.5b8d4fc2.jpg"
+  },
+  {
+    "revision": "85800c5978fab16e56d7a0c97f45b655",
+    "url": "/Love-Card/static/media/Snapchat-614847311.85800c59.jpg"
+  },
+  {
+    "revision": "e351cd7e9fbecb93d7e760b5abf6e16b",
+    "url": "/Love-Card/static/media/Snapchat-622579008.e351cd7e.jpg"
+  },
+  {
+    "revision": "4531a2236d135fae7fd4a9c6d5e56152",
+    "url": "/Love-Card/static/media/Snapchat-624856979.4531a223.jpg"
+  },
+  {
+    "revision": "595fe63616477fb259bf8a925cd9de21",
+    "url": "/Love-Card/static/media/Snapchat-62541223.595fe636.jpg"
+  },
+  {
+    "revision": "bf21bfb2d6ad34c4bdcae205fa65df5a",
+    "url": "/Love-Card/static/media/Snapchat-634522858.bf21bfb2.jpg"
+  },
+  {
+    "revision": "2d2e47dd6c477d9e6dd4de58f75e366e",
+    "url": "/Love-Card/static/media/Snapchat-64645202.2d2e47dd.jpg"
+  },
+  {
+    "revision": "f8d285d553b13f12266c94ba1250da19",
+    "url": "/Love-Card/static/media/Snapchat-751706946.f8d285d5.jpg"
+  },
+  {
+    "revision": "1a80dc79c42c7084c9d95fee35e6e3ae",
+    "url": "/Love-Card/static/media/Snapchat-846538635.1a80dc79.jpg"
+  },
+  {
+    "revision": "e8e3f8c89796899564a54506092b97ce",
+    "url": "/Love-Card/static/media/Snapchat-852908883.e8e3f8c8.jpg"
+  },
+  {
+    "revision": "68b3e8318c0e6b268e937ee869e3e53d",
+    "url": "/Love-Card/static/media/Snapchat-929353892.68b3e831.jpg"
+  },
+  {
+    "revision": "ec3db20bf76cc0b4ed608e171a4c0c71",
+    "url": "/Love-Card/static/media/Snapchat-931815948.ec3db20b.jpg"
+  }
+]);
